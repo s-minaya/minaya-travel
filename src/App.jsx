d9357941e45b1ffe1586d821ed2a611beffe9e8a
Eilith404 <minaya.sofia@gmail.com>
2026-04-20 15:49:14 +0200

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <p>Hello world</p>
+    <div className='w-16 h-16 bg-blue-700'></div>
+      
+    </>
+  )
+}
+
+export default App
