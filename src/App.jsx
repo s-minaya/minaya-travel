73448b663d3b650bf91c53b4a2a6e3e876fd9723
Eilith404 <minaya.sofia@gmail.com>
2026-04-20 17:44:18 +0200

    Add images and home section basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-    <p>Hello world</p>
-    <div className='w-16 h-16 bg-blue-700'></div>
-      
+      <nav></nav>
+      <div id="tab_bar"></div>
+      <section className="h-screen w-full">
+        <div id="home">
+          <div className="h-3/4 w-full">
+            <div className="absolute flex h-full w-full flex-col items-center space-y-96 py-4">
+              <input className="outline-none p-3 rounded-full shadow-sm" placeholder="Akihabara" type="search" name="" id="" />
+              <button className="w-36 rounded-full text-xl font-semibold p-4 bg-white text-primary shadow-sm font-potta-one">Explorar</button>
+            </div>
+            <div className="h-full w-full">
+              <img
+                src="/img/fushimi-inari-taisha.jpg"
+                alt="Imagen de Fushimi Inari Taisha"
+              />
+            </div>
+          </div>
+        </div>
+        <div id="recommended"></div>
+        <div id="featured_destinations"></div>
+      </section>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
