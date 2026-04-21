a694fde53fdaa851305f4a4501b98de2a2d6ecf0
Eilith404 <minaya.sofia@gmail.com>
2026-04-21 12:36:39 +0200

    Design destination cards UI

--- src/sections/Recommended.jsx
+++ src/sections/Recommended.jsx
@@ -1,12 +1,31 @@
 function Recommended() {
   return (
-    <div className="p-20" id="recommended">
-      <div className="h-64 w-48 rounded-lg shadow-md">
-        <div className="h-3/5 w-full rounded-t-lg bg-(image:--bg-image-yokai-matsuri) bg-cover"></div>
-        <div className="w-full h-2/5 bg-secondary rounded-bottom-lg"> 
-        <p className="text-tertiary font-nunito font-bold text-xl px-4">Yokai Matsuri</p>
-        <p className="text-primary font-potta-one text-xl px-4">妖怪祭り</p>
-        <p className="text-tertiary text-md font-nunito px-4">Festival de monstruos en Shikoku</p>
+    <div className="p-6" id="recommended">
+      <p className="text-3xl font-semibold text-primary font-nunito">Recomendados</p>
+      <div className="mt-6 flex h-72 w-auto items-center space-x-6 overflow-x-auto overscroll-x-contain">
+        <div className="Card">
+          <div className="h-3/5 w-full rounded-t-lg bg-(image:--bg-image-yokai-matsuri) bg-cover"></div>
+          <div className="rounded-bottom-lg h-2/5 w-full bg-secondary py-2 ">
+            <p className="px-4 font-nunito text-xl font-bold text-tertiary whitespace-nowrap overflow-hidden">
+              Yokai Matsuri
+            </p>
+            <p className="px-4 font-potta-one text-primary">妖怪祭り</p>
+            <p className="text-md px-4 font-nunito text-tertiary leading-tight">
+              Festival de monstruos en Shikoku
+            </p>
+          </div>
+        </div>
+        <div className="Card">
+          <div className="h-3/5 w-full rounded-t-lg bg-(image:--bg-image-nakano-broadway) bg-cover"></div>
+          <div className="rounded-bottom-lg h-2/5 w-full bg-tertiary py-2">
+            <p className="px-4 font-nunito text-xl font-bold text-secondary whitespace-nowrap overflow-hidden">
+              Nakano Broadway
+            </p>
+            <p className="px-4 font-potta-one text-primary">中野ブロードウェイ</p>
+            <p className="text-md px-4 font-nunito text-secondary leading-tight">
+              Un destino otaku al oeste de Tokio
+            </p>
+          </div>
         </div>
       </div>
     </div>
